e514e88cbfa27b263cd2c8ecaf488ac7982934df
Swetha Chakravarthy <swethachakravarthy@Swethas-MacBook-Pro.local>
2021-03-22 11:34:19 -0700

    clean up

--- api/src/controllers/InventoryController.ts
+++ api/src/controllers/InventoryController.ts
@@ -32,9 +32,8 @@ export class InventoryController {
   private async post(req: Request, res:Response) {
     /*** sample payload 
     {
-      "inventoryDate" : "2021-03-24",
-      "startTime": "01:00",
-      "endTime": "03:00",
+      "startDateTime": "2021-03-24 01:00",
+      "endDateTime": "2021-03-24 03:00",
       "allowedReservations": 2
     }   
   check the time range from_time < to_time and there are no other conflicting existing inventory records
@@ -42,14 +41,7 @@ export class InventoryController {
     let entries = []
     const startDateTime = moment(req.body.startDateTime)
     const endDateTime = moment(req.body.endDateTime)
-    // const startDateTime = moment(
-    //   req.body.inventoryDate + ' ' + req.body.startDateTime,
-    //   'YYYY-MM-DD HH:mm'
-    // )
-    // const endDateTime = moment(
-    //   req.body.inventoryDate + ' ' + req.body.startDateTime,
-    //   'YYYY-MM-DD h:mm'
-    // )
+
     let timeCounter = moment(startDateTime)
     while (endDateTime.diff(timeCounter) > 0) {
       entries.push({
@@ -82,48 +74,40 @@ export class InventoryController {
       result = res.status(StatusCodes.INTERNAL_SERVER_ERROR).jsonp(error)
     }
 
-    // var result
-    // try {
-    //   var created = await Inventory.bulkCreate(entries)
-    //   result = res.status(StatusCodes.CREATED).json(created)
-    // } catch (error) {
-    //   result = res.status(StatusCodes.INTERNAL_SERVER_ERROR)
-    // }
-
     return result
   }
 
-//  @Put(':id')
-//   private async put(req: Request, res: Response) {
-//      /*** sample payload 
-//     {
-//       "inv_used_reservations": 1
-//     } 
-//     **/
-//    let whereOptions = {}
-//     whereOptions['id'] = req.params.id
+ @Put(':id')
+  private async put(req: Request, res: Response) {
+     /*** sample payload 
+    {
+      "inv_used_reservations": 1
+    } 
+    **/
+   let whereOptions = {}
+    whereOptions['id'] = req.params.id
 
-//     try {
-//       let toUpdate = await Inventory.findOne({ where: { id: req.params.id }})
-//       let updated = toUpdate.update(req.body)
-//       return res.status(StatusCodes.CREATED).json({message: 'update_called'});
-//       }  
-//      catch(error) {
-//         return res.status(StatusCodes.NOT_FOUND).json({
-//         message: 'could not find entity with the id'
-//       })
-//     }
-//   }
+    try {
+      let toUpdate = await Inventory.findOne({ where: { id: req.params.id }})
+      let updated = toUpdate.update(req.body)
+      return res.status(StatusCodes.CREATED).json({message: 'update_called'});
+      }  
+     catch(error) {
+        return res.status(StatusCodes.NOT_FOUND).json({
+        message: 'could not find entity with the id'
+      })
+    }
+  }
 
-//   @Delete(':id')
-//   private async delete(req: Request, res: Response) {
-//     try {
-//       let toDelete = await Inventory.findOne({ where: { id: req.params.id }})
-//       await toDelete.destroy()
-//       return res.status(StatusCodes.OK).json(toDelete);
-//     } catch(error) {
-//         return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(error)
-//     }
-//   }
+  @Delete(':id')
+  private async delete(req: Request, res: Response) {
+    try {
+      let toDelete = await Inventory.findOne({ where: { id: req.params.id }})
+      await toDelete.destroy()
+      return res.status(StatusCodes.OK).json(toDelete);
+    } catch(error) {
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(error)
+    }
+  }
  
 }
